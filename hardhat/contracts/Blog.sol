03573ceb1c7e17e50159e836a87871b2dd87aecc
Matthew <alexandermatthew12@gmail.com>
2022-10-21 23:11:29 +0800

    Allow anyone to post, adding the "author" field

--- hardhat/contracts/Blog.sol
+++ hardhat/contracts/Blog.sol
@@ -13,18 +13,27 @@ contract Blog is Ownable {
     string public name;
 
     using Counters for Counters.Counter;
+    // "postId" starts incrementing from "1"
     Counters.Counter private _postIds;
 
     struct Post {
         uint256 id;
         address author;
         string title;
+        // IPFS hash
         string content;
         bool published;
+        uint256 publishedAt;
     }
 
     mapping(uint256 => Post) private idToPost;
     mapping(string => Post) private ipfsHashToPost;
+
+    modifier onlyAuthor(uint256 postId) {
+        Post storage post = idToPost[postId];
+        require(msg.sender == post.author, "Not post author");
+        _;
+    }
 
     // Events to be indexed by the Graph
     event PostCreated(
@@ -40,6 +49,12 @@ contract Blog is Ownable {
         string ipfsHash,
         bool published
     );
+    event PostPublished(
+        uint256 id,
+        address author,
+        string title,
+        string ipfsHash
+    );
 
     constructor(string memory _name) {
         console.log("Deploying a Blog with name:", _name);
@@ -54,14 +69,47 @@ contract Blog is Ownable {
         _transferOwnership(newOwner);
     }
 
-    function createPost(string memory title, string memory ipfsHash)
-        public
-        onlyOwner
-    {
+    // Fetch via IPFS content
+    function fetchPost(string memory hash) public view returns (Post memory) {
+        return ipfsHashToPost[hash];
+    }
+
+    function fetchPostsByAuthor() public view returns (Post[] memory) {
+        uint256 numPosts = _postIds.current();
+        uint256 numPostsByAuthor = 0;
+
+        // Calculate "numPostsByAuthor" since memory array is static in length
+        for (uint256 id = 1; id <= numPosts; id++) {
+            Post memory post = idToPost[id];
+            if (post.author == msg.sender) {
+                numPostsByAuthor++;
+            }
+        }
+
+        Post[] memory postsByAuthor = new Post[](numPostsByAuthor);
+        // Create the "fetchPostsByAuthor"
+        for (uint256 id = 1; id <= numPostsByAuthor; id++) {
+            Post memory post = idToPost[id];
+            if (post.author == msg.sender) {
+                postsByAuthor[id - 1] = post;
+            }
+        }
+
+        return postsByAuthor;
+    }
+
+    function createPost(string memory title, string memory ipfsHash) public {
         _postIds.increment();
         uint256 postId = _postIds.current();
 
-        Post memory post = Post(postId, msg.sender, title, ipfsHash, false);
+        Post memory post = Post({
+            id: postId,
+            author: msg.sender,
+            title: title,
+            content: ipfsHash,
+            published: false,
+            publishedAt: 0
+        });
 
         idToPost[postId] = post;
         ipfsHashToPost[ipfsHash] = post;
@@ -69,31 +117,39 @@ contract Blog is Ownable {
         emit PostCreated(postId, post.author, title, ipfsHash);
     }
 
+    function publishPost(uint256 postId) public onlyAuthor(postId) {
+        Post storage post = idToPost[postId];
+        require(post.id > 0, "Post not found");
+
+        post.publishedAt = block.timestamp;
+        idToPost[postId] = post;
+
+        emit PostPublished(postId, post.author, post.title, post.content);
+    }
+
     function updatePost(
         uint256 postId,
         string memory title,
-        string memory ipfsHash,
-        bool published
-    ) public onlyOwner {
+        string memory ipfsHash
+    ) public onlyAuthor(postId) {
         Post storage post = idToPost[postId];
 
         require(post.id > 0, "Post not found");
-        require(post.author == msg.sender, "Only author can modify their post");
 
         post.title = title;
-        post.published = published;
         post.content = ipfsHash;
 
         idToPost[postId] = post;
         ipfsHashToPost[ipfsHash] = post;
 
-        emit PostUpdated(postId, post.author, title, ipfsHash, published);
+        emit PostUpdated(postId, post.author, title, ipfsHash, post.published);
     }
 
     function fetchPosts() public view returns (Post[] memory) {
         uint256 numPosts = _postIds.current();
 
-        // Dynamic array
+        // Not possible to resize memory arrays
+        // Only possible to resize storage arrays
         Post[] memory posts = new Post[](numPosts);
         for (uint256 id = 1; id <= numPosts; id++) {
             Post storage post = idToPost[id];
